c845e98889f1d8b698732f3d3faadb06ec74ae66
Leo <leo.pan.duh@gmail.com>
2019-06-17 15:39:37 -0400

    add resume link

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import DigitalClock from "./DigitalClock";
 import Title from "./Title";
 import "../style.css";
+import resumepdf from "../assets/resume.pdf";
 
 class App extends React.Component {
   goToAbout = event => {
@@ -13,8 +14,16 @@ class App extends React.Component {
       <div className="app">
         <DigitalClock />
         <Title />
-        <button className="me-button" onClick={() => this.goToAbout()} />
-        <button className="resume-button" />
+        <button
+          className="me-button"
+          title="about me"
+          onClick={() => this.goToAbout()}
+        />
+        <button
+          className="resume-button"
+          title="resume"
+          onClick={() => window.open(resumepdf)}
+        />
       </div>
     );
   }
